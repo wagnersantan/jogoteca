648234a06c6fc93acf399fe7d8da36769d12f2c8
Wagner <wagneramparo@gmail.com>
2025-04-24 23:58:33 -0300

    Adiciona autenticação com sessão e template de login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,session,flash
 
 # Definindo a classe do jogo
 class Jogo:
@@ -9,6 +9,7 @@ class Jogo:
 
 # Instância do app Flask
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 # Lista de jogos (pode ser usada em várias rotas futuramente)
 jogos = [
@@ -44,7 +45,23 @@ def criar():
 
     # Redireciona para a página inicial após salvar
     return redirect('/inicio')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+
+    if senha == 'alohomora':
+        session['usuario_logado'] = usuario
+        flash(f'{usuario} logado com sucesso')
+        return redirect('/')
+    else:
+        flash('Usuário não logado')
+        return redirect('/login')
+    
 # Execução do servidor Flask
 if __name__ == '__main__':
     app.run(debug=True)
